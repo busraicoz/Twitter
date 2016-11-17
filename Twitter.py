494c849a7bce6ca1eddd69b8a3fd14e3ccf5e797
busraicoz <icoz_bsr@hotmail.com>
2016-11-17 23:42:38 +0200

    Add files via upload

---
+++ Twitter.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Retrieving data from twitter with specific keyword and geo location by using Twython
+import json
+from twython import Twython
+
+def retreive_data_kw(keyword):     #retreive data with specific keyword
+
+    TWITTER_APP_KEY = '********'
+    TWITTER_APP_KEY_SECRET ='****************'
+    TWITTER_ACCESS_TOKEN = '*****************'
+    TWITTER_ACCESS_TOKEN_SECRET = '***********'
+
+    t = Twython(app_key=TWITTER_APP_KEY,
+            app_secret=TWITTER_APP_KEY_SECRET,
+            oauth_token=TWITTER_ACCESS_TOKEN,
+            oauth_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+    search = t.search(q=keyword,count=100)
+    tweets = search['statuses']
+    file = open("output.json", "w")
+    for tweet in tweets:
+        retweet=tweet['retweet_count']
+        fav=tweet['favorite_count']
+        screenname = "@" + tweet['user']['screen_name']
+        id = tweet['id_str']
+        text = tweet['text']
+        date=tweet['created_at']
+        data = { "id": id, "date":date, "name": screenname,"tweet": tweet['text'], "values":{"favorite":fav, "retweet": retweet}}
+        print (json.dumps(data, indent=3, ensure_ascii=False)).encode('utf8')
+        file.write(json.dumps(data, indent=3, ensure_ascii=False).encode('utf8'))
+
+def retreive_data_geocode(geocode):     #retreive data with specific location
+
+        TWITTER_APP_KEY = '9GkEMR8SEzmDndbKFgUSLGPDU'
+        TWITTER_APP_KEY_SECRET = 'pcQrh8T3F5y2vBhjsKXS7cuEqdBCGH9ADha8QvK19NAzRBL0SA'
+        TWITTER_ACCESS_TOKEN = "790611908973985792-9TJs07YhBy2qz0b2llLwZpSBzPGz0C2"
+        TWITTER_ACCESS_TOKEN_SECRET = "wNVQqLTBwuhPreNCIucayqrmQlyrXyP5QKAjdfxN8UE7Y"
+
+        t = Twython(app_key=TWITTER_APP_KEY,
+                    app_secret=TWITTER_APP_KEY_SECRET,
+                    oauth_token=TWITTER_ACCESS_TOKEN,
+                    oauth_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+        search = t.search(geocode=geocode, count=100)
+        tweets = search['statuses']
+        file = open("output.txt", "w")
+        for tweet in tweets:
+            retweet = tweet['retweet_count']
+            fav = tweet['favorite_count']
+            screenname = "@" + tweet['user']['screen_name']
+            id = tweet['id_str']
+            text = tweet['text']
+            date = tweet['created_at']
+            data = {"id": id, "date": date, "name": screenname, "tweet": tweet['text'],
+                    "values": {"favorite": fav, "retweet": retweet}}
+            print (json.dumps(data, indent=3, ensure_ascii=False)).encode('utf8')
+            file.write(json.dumps(data, indent=3, ensure_ascii=False).encode('utf8'))
+
+retreive_data_kw("YBU")
+retreive_data_kw("#AYBU")
+retreive_data_geocode('39.9708048,32.8187681,0.1mi')
+
+
